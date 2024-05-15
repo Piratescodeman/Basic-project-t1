9017c470a7286829c57bc19fecf57db26fa02e6b
Piratescodeman <vishnubcomca23@gmail.com>
2024-05-15 22:14:40 +0530

    Update color_detection.py

--- color_detection.py
+++ color_detection.py
@@ -1,47 +1,69 @@
+# Dataset - https://github.com/codebrainz/color-names/blob/master/output/colors.csv
+
 import cv2
-import numpy as np
 import pandas as pd
-from sklearn.cluster import KMeans
 
-# Function to get color name from RGB values
-def get_color_name(R, G, B, color_data):
-    minimum = 1000
-    color_name = ''
-    for i in range(len(color_data)):
-        d = abs(R - color_data.loc[i, 'R']) + abs(G - color_data.loc[i, 'G']) + abs(B - color_data.loc[i, 'B'])
-        if d < minimum:
-            minimum = d
-            color_name = color_data.loc[i, 'color_name']
-    return color_name
-
-# Function to display the dominant colors and their names
-def display_colors(colors, color_data):
-    img_colors = np.zeros((100, len(colors) * 100, 3), dtype=np.uint8)
-    for i, color in enumerate(colors):
-        img_colors[:, i*100:(i+1)*100] = color
-        color_name = get_color_name(color[2], color[1], color[0], color_data)
-        cv2.putText(img_colors, color_name, (i*100, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    cv2.imshow('Dominant Colors', img_colors)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-# Load image
-img_path = 'pic2.jpg'
+img_path = r'C:\Users\Balaji\Downloads\color detection\colorpic.jpg'
 img = cv2.imread(img_path)
 
-# Reshape image
-pixels = img.reshape((-1, 3))
-pixels = np.float32(pixels)
+# declaring global variables (are used later on)
+clicked = False
+r = g = b = x_pos = y_pos = 0
 
-# K-means clustering
-num_colors = 5
-kmeans = KMeans(n_clusters=num_colors)
-kmeans.fit(pixels)
-colors = kmeans.cluster_centers_
-colors = np.uint8(colors)
+# Reading csv file with pandas and giving names to each column
+index = ["color", "color_name", "hex", "R", "G", "B"]
+csv = pd.read_csv('colors.csv', names=index, header=None)
 
-# Load color data from CSV
-color_data = pd.read_csv('colors.csv')
 
-# Display dominant colors
-display_colors(colors, color_data)
+# function to calculate minimum distance from all colors and get the most matching color
+def get_color_name(R, G, B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+        if d <= minimum:
+            minimum = d
+            cname = csv.loc[i, "color_name"]
+    return cname
+
+
+# function to get x,y coordinates of mouse double click
+def draw_function(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b, g, r, x_pos, y_pos, clicked
+        clicked = True
+        x_pos = x
+        y_pos = y
+        b, g, r = img[y, x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+
+
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', draw_function)
+
+while True:
+
+    cv2.imshow("image", img)
+    if clicked:
+
+        # cv2.rectangle(image, start point, endpoint, color, thickness)-1 fills entire rectangle
+        cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
+
+        # Creating text string to display( Color name and RGB values )
+        text = get_color_name(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
+
+        # cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
+        cv2.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # For very light colours we will display text in black colour
+        if r + g + b >= 600:
+            cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
+
+        clicked = False
+
+    # Break the loop when user hits 'esc' key
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
